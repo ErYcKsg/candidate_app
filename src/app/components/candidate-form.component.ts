5fd4a4bbf91a6c54da8e0fe45a39329800460a0b
Eric Salguero <eric.salguero.87@gmail.com>
2025-05-24 13:17:34 +0200

    feat: apply some visual changes

--- src/app/components/candidate-form.component.ts
+++ src/app/components/candidate-form.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { CandidateDataSource } from '../services/candidate-data-source';
 
 @Component({
   selector: 'app-candidate-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  styleUrls: ['./candidate-form.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './candidate-form.component.html',
 })
 export class CandidateFormComponent {
